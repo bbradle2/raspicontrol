db3ecf663dfb6b757d0f18b9e5d2bebbfb0945d7
Brian Bradley <bbradle2@gmail.com>
2025-11-07 15:07:22 -0700

    latest changes

--- src/gpioController.cpp
+++ src/gpioController.cpp
@@ -4,6 +4,19 @@ using namespace std;
 
 gpioController::gpioController()
 {
+    // auto lineTest = gpiod::find_line("GPIO24");
+    // std::cout << lineTest.name() << "  " << lineTest.offset() << std::endl;
+    // for (const auto &chip : gpiod::make_chip_iter())
+    // {
+    //     std::cout << chip.name() << " (" << chip.label() << ") " << "(" << chip.num_lines() << ")" << std::endl;
+
+    //     for(auto line : chip.get_all_lines())
+    //     {
+    //         std::cout << "   " << line.name() << "  " << line.offset()  << std::endl;
+    //     }
+    // }
+    // std::cout << std::endl;
+
     _chip.open(RaspberryPiDefines::CHIP_NAME);
 
     _gpios.emplace(RaspberryPiDefines::GPIO23, new gpioObject{_chip, RaspberryPiDefines::PIN23, GPIOD_LINE_REQUEST_DIRECTION_OUTPUT});
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <main.hpp>
+#include <gpiod.hpp>
 
 int main(int argc, char **argv)
 {
